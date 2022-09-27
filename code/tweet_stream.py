608a895a42cd9ef74e756d5f638353f9bf6d85ef
Ahmed Morsi <amoursi@miu.edu>
2022-09-27 02:08:10 -0400

    Update tweet_stream.py

--- code/tweet_stream.py
+++ code/tweet_stream.py
@@ -1,26 +1,56 @@
-#!/usr/bin/python3                                                                                                      
-                                                                                                                        
-from kafka import KafkaProducer                                                                                         
-from random import randint                                                                                              
-from time import sleep                                                                                                  
-import sys                                                                                                              
-                                                                                                                        
-BROKER = 'localhost:9092'                                                                                               
-TOPIC = 'tweets'                                                                                                      
-                                                                                                                        
-WORD_FILE = '/usr/share/dict/words'                                                                                     
-WORDS = open(WORD_FILE).read().splitlines()                                                                             
-                                                                                                                        
-try:                                                                                                                    
-    p = KafkaProducer(bootstrap_servers=BROKER)                                                                         
-except Exception as e:                                                                                                  
-    print(f"ERROR --> {e}")                                                                                             
-    sys.exit(1)                                                                                                        
-                                                                                                                        
-while True:                                                                                                             
-    message = ''                                                                                                        
-    for _ in range(randint(2, 7)):                                                                                      
-        message += WORDS[randint(0, len(WORDS)-1)] + ' '                                                                
-    print(f">>> '{message}'")                                                                                           
-    p.send(TOPIC, bytes(message, encoding="utf8"))                                                                      
-    sleep(randint(1,4))
+#!/usr/bin/python3 
+
+from kafka import KafkaProducer
+from datetime import datetime
+import tweepy
+import sys
+import re
+
+TWEET_TOPICS = ['bitcoin']
+
+KAFKA_BROKER = 'localhost:9092'
+KAFKA_TOPIC = 'tweets'
+
+class Streamer(tweepy.Stream):
+
+    def on_error(self, status_code):
+        if status_code == 402:
+            return False
+
+    def on_status(self, status):
+        tweet = status.text
+
+        tweet = re.sub(r'RT\s@\w*:\s', '', tweet)
+        tweet = re.sub(r'https?.*', '', tweet)
+
+        global producer
+        producer.send(KAFKA_TOPIC, bytes(tweet, encoding='utf-8'))
+
+        d = datetime.now()
+
+        print(f'[{d.hour}:{d.minute}.{d.second}] sending tweet')
+
+# put your API keys here
+consumer_key = "consumer_key"
+consumer_secret_key = "consumer_secret_key"
+
+access_token = "access_token"
+access_token_secret = "access_token_secret"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+stream = Streamer(consumer_key,
+                          consumer_secret_key,
+                          access_token,
+                          access_token_secret)
+
+try:
+    producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
+except Exception as e:
+    print(f'Error Connecting to Kafka --> {e}')
+    sys.exit(1)
+
+stream.filter(track=TWEET_TOPICS)
